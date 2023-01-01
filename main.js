fa63c2e4cb2b166ce21c28bdba0bf393fbacf615
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2022-12-31 21:12:16 -0300

    :lipstick: Update the UI in body and html

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import "./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import "./src/styles/elements/base.css";
 import BoardGame from "./src/objects/BoardGame";
+import PlayerMoves from "./src/components/PlayerMoves";
 
 const $root = document.querySelector("#root");
 
-$root.insertAdjacentHTML("beforeend", BoardGame(20));
+$root.insertAdjacentHTML("beforeend", PlayerMoves() + BoardGame(20));
