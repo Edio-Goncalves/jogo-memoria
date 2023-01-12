40b5b87488c65b97b229945bdc306706f24f7e3a
Edio Goncalves <ediogoncalvesjr@hotmail.com>
2023-01-12 13:39:35 -0300

    Create the link page in login js

--- js/login.js
+++ js/login.js
@@ -1,21 +1,20 @@
-const input = document.querySelector(".login-input");
-const button = document.querySelector(".login-button");
-const form = document.querySelector(".login-form");
+const inputEvent = document.querySelector(".login-input");
+const buttonEvent = document.querySelector(".login-button");
+const formEvent = document.querySelector(".login-form");
 
 function checkInput(event) {
   if (event.target.value.length > 3) {
-    button.removeAttribute("disabled");
+    buttonEvent.removeAttribute("disabled");
   } else {
-    button.setAttribute("disabled", "");
+    buttonEvent.setAttribute("disabled", "");
   }
-  console.log(event.target.value.length);
 }
 
-function submitForm(event) {
+function submitButton(event) {
   event.preventDefault();
-  localStorage.setItem("player", input.value);
+  localStorage.setItem("player", inputEvent.value);
   window.location = "/pages/game.html";
 }
 
-input.addEventListener("input", checkInput);
-form.addEventListener("submit", submitForm);
+inputEvent.addEventListener("input", checkInput);
+formEvent.addEventListener("submit", submitButton);
